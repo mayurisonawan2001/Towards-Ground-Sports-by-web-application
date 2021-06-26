f8fd058329b705a3cf08c4597419dc6d1b5d3f18
mayurisonawan2001 <75942262+mayurisonawan2001@users.noreply.github.com>
2021-06-26 20:47:31 +0530

    Create post.php

---
+++ post.php
@@ -0,0 +1,10 @@
+# Towards-Ground-Sports-by-web-application
+<?php
+session_start();
+if(isset($_SESSION['name'])){
+    $text = $_POST['text'];
+    date_default_timezone_set('Asia/Calcutta');
+    $text_message = "<div class='msgln'><span class='chat-time'>".date('Y-m-d H:i:s')."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+}
+?>
